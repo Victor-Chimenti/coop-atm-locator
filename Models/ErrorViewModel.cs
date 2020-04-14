9bd8d5baf0471c9a73dd8146148d6a57ae1e13bf
Vic Chimenti <chimenti@seattleu.edu>
2020-04-14 11:23:20 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coop_atm_locator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
