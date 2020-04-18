6ae14a35f96da00366eda942333f9f087c0c3ca4
Vic Chimenti <chimenti@seattleu.edu>
2020-04-18 09:38:58 -0700

    reverse scaffold

---
+++ Models/Locations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace coop_atm_locator.Models
+{
+    public partial class Locations
+    {
+        public string ReferenceId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string WebAddress { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Hours { get; set; }
+        public string RetailOutlet { get; set; }
+        public string RestrictedAccess { get; set; }
+        public string AcceptDeposit { get; set; }
+        public string AcceptCash { get; set; }
+        public string EnvelopeRequired { get; set; }
+        public string OnMilitaryBase { get; set; }
+        public string OnPremise { get; set; }
+        public string Surcharge { get; set; }
+        public string Access { get; set; }
+        public string AccessNotes { get; set; }
+        public string InstallationType { get; set; }
+        public string HandicapAccess { get; set; }
+        public string LocationType { get; set; }
+        public string HoursMonOpen { get; set; }
+        public string HoursMonClose { get; set; }
+        public string HoursTueOpen { get; set; }
+        public string HoursTueClose { get; set; }
+        public string HoursWedOpen { get; set; }
+        public string HoursWedClose { get; set; }
+        public string HoursThuOpen { get; set; }
+        public string HoursThuClose { get; set; }
+        public string HoursFriOpen { get; set; }
+        public string HoursFriClose { get; set; }
+        public string HoursSatOpen { get; set; }
+        public string HoursSatClose { get; set; }
+        public string HoursSunOpen { get; set; }
+        public string HoursSunClose { get; set; }
+        public string HoursDtmonOpen { get; set; }
+        public string HoursDtmonClose { get; set; }
+        public string HoursDttueOpen { get; set; }
+        public string HoursDttueClose { get; set; }
+        public string HoursDtwedOpen { get; set; }
+        public string HoursDtwedClose { get; set; }
+        public string HoursDtthuOpen { get; set; }
+        public string HoursDtthuClose { get; set; }
+        public string HoursDtfriOpen { get; set; }
+        public string HoursDtfriClose { get; set; }
+        public string HoursDtsatOpen { get; set; }
+        public string HoursDtsatClose { get; set; }
+        public string HoursDtsunOpen { get; set; }
+        public string HoursDtsunClose { get; set; }
+        public string Cashless { get; set; }
+        public string DriveThruOnly { get; set; }
+        public string LimitedTransactions { get; set; }
+        public string MilitaryIdRequired { get; set; }
+        public string SelfServiceDevice { get; set; }
+        public string SelfServiceOnly { get; set; }
+    }
+}
---
+++ Models/atmLocator_flatContext.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace coop_atm_locator.Models
+{
+    public partial class atmLocator_flatContext : DbContext
+    {
+        public atmLocator_flatContext()
+        {
+        }
+
+        public atmLocator_flatContext(DbContextOptions<atmLocator_flatContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Locations> Locations { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Locations>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.AcceptCash).HasMaxLength(50);
+
+                entity.Property(e => e.AcceptDeposit).HasMaxLength(50);
+
+                entity.Property(e => e.Access).HasMaxLength(50);
+
+                entity.Property(e => e.AccessNotes)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Cashless)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.County)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.DriveThruOnly)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.EnvelopeRequired).HasMaxLength(50);
+
+                entity.Property(e => e.Fax)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HandicapAccess).HasMaxLength(50);
+
+                entity.Property(e => e.Hours)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtfriClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTFriClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtfriOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTFriOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtmonClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTMonClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtmonOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTMonOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtsatClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTSatClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtsatOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTSatOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtsunClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTSunClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtsunOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTSunOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtthuClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTThuClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtthuOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTThuOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDttueClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTTueClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDttueOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTTueOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtwedClose)
+                    .IsRequired()
+                    .HasColumnName("HoursDTWedClose")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursDtwedOpen)
+                    .IsRequired()
+                    .HasColumnName("HoursDTWedOpen")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursFriClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursFriOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursMonClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursMonOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursSatClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursSatOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursSunClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursSunOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursThuClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursThuOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursTueClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursTueOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursWedClose)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.HoursWedOpen)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.InstallationType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Latitude).HasColumnType("decimal(9, 6)");
+
+                entity.Property(e => e.LimitedTransactions)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LocationType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Longitude).HasColumnType("decimal(9, 6)");
+
+                entity.Property(e => e.MilitaryIdRequired)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.OnMilitaryBase)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.OnPremise).HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PostalCode)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ReferenceId)
+                    .IsRequired()
+                    .HasColumnName("ReferenceID")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.RestrictedAccess)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.RetailOutlet)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.SelfServiceDevice)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.SelfServiceOnly)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Surcharge).HasMaxLength(50);
+
+                entity.Property(e => e.WebAddress)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
