65d9136cc178b7d61ad4411b204b2b75463ec2a5
Vic Chimenti <chimenti@seattleu.edu>
2020-04-19 09:39:15 -0700

    add position model class

---
+++ Models/PositionModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coop_atm_locator.Models
+{
+    public class PositionModel
+    {
+        public double Lat { get; set; } = 0.0;
+        public double Lng { get; set; } = 0.0;
+
+
+
+        /// <summary>
+        /// Convert from String to Position
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lng"></param>
+        public PositionModel(string lat, string lng)
+        {
+            try
+            {
+                Lat = Convert.ToDouble(lat);
+                Lng = Convert.ToDouble(lng);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Convert from Decimal to Position
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lng"></param>
+        public PositionModel(decimal lat, decimal lng)
+        {
+            try
+            {
+                Lat = Convert.ToDouble(lat);
+                Lng = Convert.ToDouble(lng);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
