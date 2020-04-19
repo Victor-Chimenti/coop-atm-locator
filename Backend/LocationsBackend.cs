34bd21d4b240a67d00fc55df00abbff0138d4213
Vic Chimenti <chimenti@seattleu.edu>
2020-04-19 10:32:49 -0700

    add locations backend

---
+++ Backend/LocationsBackend.cs
@@ -0,0 +1,29 @@
+﻿using coop_atm_locator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coop_atm_locator.Backend
+{
+    public class LocationsBackend
+    {
+        private atmLocator_flatContext context;
+        //private int TakeSize;
+        //private int TakeIndex;
+        //private PositionModel point;
+
+        public LocationsBackend(atmLocator_flatContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual async Task<List<Locations>> IndexAsync()
+        {
+
+            var locations_list = await context.ReadMultipleRecordsAsync().ConfigureAwait(false); // Select * join all tables
+
+            return locations_list;
+        }
+    }
+}
